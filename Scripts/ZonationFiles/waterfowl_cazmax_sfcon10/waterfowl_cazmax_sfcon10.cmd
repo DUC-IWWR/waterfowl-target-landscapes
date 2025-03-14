f59e4e74a49ae6dd68ebcf22fb6f1e6a0fc19795
Barry Robinson <barry.robinson@ec.gc.ca>
2025-03-14 11:12:24 -0600

    Deleting old Zonation scenarios no longer needed

--- Scripts/ZonationFiles/waterfowl_cazmax_sfcon10/waterfowl_cazmax_sfcon10.cmd
+++
@@ -1,8 +0,0 @@
-@setlocal
-@PATH=C:\Program Files (x86)\Zonation5;%PATH%
-
-z5_16bit -ad --mode=CAZMAX --gui waterfowl_cazmax_sfcon10.z5 ../../../Output/ZonationRaw/waterfowl_cazmax_sfcon10
-#-a = analysis area mask, which will exlude any pixels that don't have data for all species
-#-d = single feature connectivity
-
-@pause
