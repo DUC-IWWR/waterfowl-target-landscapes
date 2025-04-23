a534b56fcb76482e23b56eff6c9dc097b9873c64
Brandon Edwards <b_edwards@ducks.ca>
2025-04-23 11:35:32 -0500

    WIP conversion to using geotargets, unsure if this will actually be a useful thing here due to the need of file paths for Zonation

--- _targets.R
+++ _targets.R
@@ -26,226 +26,202 @@ tar_option_set(
 list(
   
   ####### Raw Rasters ###################################
-  tar_target(
-    name = species_7_stacked,
-    "data/raw/rasters/7species_perSQK_CopyRaster.tif",
-    format = "file"
+  tar_terra_rast(
+    name = species_stacked_raw,
+    terra::rast("data/raw/rasters/7species_perSQK_CopyRaster.tif")
   ),
-  tar_target(
+  tar_terra_rast(
     name = mall_raw_raster,
-    "data/raw/rasters/MALL_perSQK_CopyRaster.tif",
-    format = "file"
+    terra::rast("data/raw/rasters/MALL_perSQK_CopyRaster.tif")
   ),
-  tar_target(
+  tar_terra_rast(
     name = gadw_raw_raster,
-    "data/raw/rasters/GADW_perSQK_CopyRaster.tif",
-    format = "file"
+    terra::rast("data/raw/rasters/GADW_perSQK_CopyRaster.tif")
   ),
-  tar_target(
+  tar_terra_rast(
     name = nopi_raw_raster,
-    "data/raw/rasters/NOPI_perSQK_CopyRaster.tif",
-    format = "file"
+    terra::rast("data/raw/rasters/NOPI_perSQK_CopyRaster.tif")
   ),
-  tar_target(
+  tar_terra_rast(
     name = bwte_raw_raster,
-    "data/raw/rasters/BWTE_Pairs_perSQK_CopyRaster.tif",
-    format = "file"
+    terra::rast("data/raw/rasters/BWTE_Pairs_perSQK_CopyRaster.tif")
   ),
-  tar_target(
+  tar_terra_rast(
     name = nsho_raw_raster,
-    "data/raw/rasters/NSHO_perSQK_CopyRaster.tif",
-    format = "file"
+    terra::rast("data/raw/rasters/NSHO_perSQK_CopyRaster.tif")
   ),
-  tar_target(
+  tar_terra_rast(
     name = canv_raw_raster,
-    "data/raw/rasters/CANV_Pairs_perSQK_CopyRaster.tif",
-    format = "file"
+    terra::rast("data/raw/rasters/CANV_Pairs_perSQK_CopyRaster.tif")
   ),
-  tar_target(
+  tar_terra_rast(
     name = redh_raw_raster,
-    "data/raw/rasters/REDH_perSQK_CopyRaster.tif",
-    format = "file"
+    terra::rast("data/raw/rasters/REDH_perSQK_CopyRaster.tif")
   ),
-  tar_target(
+  tar_terra_vect(
     name = tl_old,
-    "data/raw/target-landscapes-previous/PHJV_WaterfowlTargetLandscapes.shp",
-    format = "file"
+    terra::vect("data/raw/target-landscapes-previous/PHJV_WaterfowlTargetLandscapes.shp")
   ),
   
   
   ####### Reprojected Rasters ###################################
-  tar_target(
-    name = species_7_reprojected,
-    command = reproject_raster(raster_file = species_7_stacked,
-                               ref = gadw_raw_raster),
-    format = "file"
+  # Here we choose to reproject based on the geometry of the GADW raster.
+  tar_terra_rast(
+    name = species_stacked_rast,
+    command = terra::project(species_stacked_raw, gadw_raw_raster)
   ),
-  tar_target(
-    name = mall_reprojected,
-    command = reproject_raster(raster_file = mall_raw_raster,
-                               ref = gadw_raw_raster),
-    format = "file"
+  tar_terra_rast(
+    name = mall_rast,
+    command = terra::project(mall_raw_raster, gadw_raw_raster)
   ),
-  tar_target(
-    name = gadw_reprojected,
-    command = reproject_raster(raster_file = gadw_raw_raster,
-                               ref = gadw_raw_raster),
-    format = "file"
+  tar_terra_rast(
+    name = gadw_rast,
+    command = terra::project(gadw_raw_raster, gadw_raw_raster)
   ),
-  tar_target(
-    name = nopi_reprojected,
-    command = reproject_raster(raster_file = nopi_raw_raster,
-                               ref = gadw_raw_raster),
-    format = "file"
+  tar_terra_rast(
+    name = nopi_rast,
+    command = terra::project(nopi_raw_raster, gadw_raw_raster)
   ),
-  tar_target(
-    name = bwte_reprojected,
-    command = reproject_raster(raster_file = bwte_raw_raster,
-                               ref = gadw_raw_raster),
-    format = "file"
+  tar_terra_rast(
+    name = bwte_rast,
+    command = terra::project(bwte_raw_raster, gadw_raw_raster)
   ),
-  tar_target(
-    name = nsho_reprojected,
-    command = reproject_raster(raster_file = nsho_raw_raster,
-                               ref = gadw_raw_raster),
-    format = "file"
+  tar_terra_rast(
+    name = nsho_rast,
+    command = terra::project(nsho_raw_raster, gadw_raw_raster)
   ),
-  tar_target(
-    name = canv_reprojected,
-    command = reproject_raster(raster_file = canv_raw_raster,
-                               ref = gadw_raw_raster),
-    format = "file"
+  tar_terra_rast(
+    name = canv_rast,
+    command = terra::project(canv_raw_raster, gadw_raw_raster)
   ),
-  tar_target(
-    name = redh_reprojected,
-    command = reproject_raster(raster_file = redh_raw_raster,
-                               ref = gadw_raw_raster),
-    format = "file"
-  ),
-  
-  ####### Layer Stacking Targets ###################################
-  
-  # note that "dabblers_stacked" does NOT include pintail
-  tar_target(
-    name = dabblers_stacked,
-    command = stack_rasters(raster_list = c(mall_reprojected,
-                                            gadw_reprojected,
-                                            bwte_reprojected,
-                                            nsho_reprojected),
-                            new_raster_name = "dabblers"),
-    format = "file"
-  ),
-  
-  tar_target(
-    name = divers_stacked,
-    command = stack_rasters(raster_list = c(canv_reprojected,
-                                            redh_reprojected),
-                            new_raster_name = "divers"),
-    format = "file"
-  ),
-  
-  ####### Zonation Scenarios ###################################
-  
-  ######### Single Layer #################
-  tar_target(
-    name = single_layer_rankmap,
-    command = run_zonation(feature_list = c(species_7_reprojected),
-                           scenario_name = "single_layer_all",
-                           zonation_mode = "CAZMAX"),
-    format = "file"
-  ),
-  
-   tar_target(
-    name = single_layer_tl,
-    command = generate_target_landscape(rankmap_path = single_layer_rankmap,
-                                        scenario_name = "single_layer_all",
-                                        threshold = 0.74,
-                                        min_poly = 156000000,
-                                        max_hole = 70000000,
-                                        smooth = 8),
-    format = "file"
-  ),
-  
-  # tar_target(
-  #   name = single_layer_tl_plot,
-  #   command = plot_target_landscape(tl_path = single_layer_tl,
-  #                                   old_tl_path = tl_old,
-  #                                   width = 6,
-  #                                   height = 6,
-  #                                   res = 300,
-  #                                   units = "in",
-  #                                   scenario_name = "single_layer_all")
-  # ),
-  
-  ##### Separate Layers ######
-  tar_target(
-    name = separate_layer_rankmap,
-    command = run_zonation(feature_list = c(mall_reprojected,
-                                            gadw_reprojected,
-                                            nopi_reprojected,
-                                            bwte_reprojected,
-                                            nsho_reprojected,
-                                            canv_reprojected,
-                                            redh_reprojected),
-                           scenario_name = "separate_layer_all",
-                           zonation_mode = "CAZMAX"),
-    format = "file"
-  ),
-  
-  tar_target(
-    name = separate_layer_tl,
-    command = generate_target_landscape(rankmap_path = separate_layer_rankmap,
-                                        scenario_name = "separate_layer_all",
-                                        threshold = 0.74,
-                                        min_poly = 156000000,
-                                        max_hole = 70000000,
-                                        smooth = 8),
-    format = "file"
-  ),
-  
-  # tar_target(
-  #   name = separate_layer_tl_plot,
-  #   command = plot_target_landscape(tl_path = separate_layer_tl,
-  #                                   old_tl_path = tl_old,
-  #                                   width = 6,
-  #                                   height = 6,
-  #                                   res = 300,
-  #                                   units = "in",
-  #                                   scenario_name = "separate_layer_all")
-  # ),
-  
-  ###### Guild Level Scenario #####
-  # i.e., a layer for dabblers, a layer for divers, and a layer for NOPI
-  tar_target(
-    name = guild_level_rankmap,
-    command = run_zonation(feature_list = c(dabblers_stacked,
-                                            divers_stacked,
-                                            nopi_reprojected),
-                           scenario_name = "guild_level",
-                           zonation_mode = "CAZMAX"),
-    format = "file"
-  ),
-  
-  tar_target(
-    name = guild_level_tl,
-    command = generate_target_landscape(rankmap_path = guild_level_rankmap,
-                                        scenario_name = "guild_level",
-                                        threshold = 0.74,
-                                        min_poly = 156000000,
-                                        max_hole = 70000000,
-                                        smooth = 8),
-    format = "file"
+  tar_terra_rast(
+    name = redh_rast,
+    command = terra::project(redh_raw_raster, gadw_raw_raster)
   )
   
+  ####### Layer Stacking Targets ###################################
+  # 
+  # # note that "dabblers_stacked" does NOT include pintail
   # tar_target(
-  #   name = guild_level_tl_plot,
-  #   command = plot_target_landscape(tl_path = guild_level_tl,
-  #                                   old_tl_path = tl_old,
-  #                                   width = 6,
-  #                                   height = 6,
-  #                                   res = 300,
-  #                                   units = "in",
-  #                                   scenario_name = "guild_level")
+  #   name = dabblers_stacked,
+  #   command = stack_rasters(raster_list = c(mall_reprojected,
+  #                                           gadw_reprojected,
+  #                                           bwte_reprojected,
+  #                                           nsho_reprojected),
+  #                           new_raster_name = "dabblers"),
+  #   format = "file"
+  # ),
+  # 
+  # tar_target(
+  #   name = divers_stacked,
+  #   command = stack_rasters(raster_list = c(canv_reprojected,
+  #                                           redh_reprojected),
+  #                           new_raster_name = "divers"),
+  #   format = "file"
+  # ),
+  # 
+  # ####### Zonation Scenarios ###################################
+  # 
+  # ######### Single Layer #################
+  # tar_target(
+  #   name = single_layer_rankmap,
+  #   command = run_zonation(feature_list = c(species_7_reprojected),
+  #                          scenario_name = "single_layer_all",
+  #                          zonation_mode = "CAZMAX"),
+  #   format = "file"
+  # ),
+  # 
+  #  tar_target(
+  #   name = single_layer_tl,
+  #   command = generate_target_landscape(rankmap_path = single_layer_rankmap,
+  #                                       scenario_name = "single_layer_all",
+  #                                       threshold = 0.74,
+  #                                       min_poly = 156000000,
+  #                                       max_hole = 70000000,
+  #                                       smooth = 8),
+  #   format = "file"
+  # ),
+  # 
+  # # tar_target(
+  # #   name = single_layer_tl_plot,
+  # #   command = plot_target_landscape(tl_path = single_layer_tl,
+  # #                                   old_tl_path = tl_old,
+  # #                                   width = 6,
+  # #                                   height = 6,
+  # #                                   res = 300,
+  # #                                   units = "in",
+  # #                                   scenario_name = "single_layer_all")
+  # # ),
+  # 
+  # ##### Separate Layers ######
+  # tar_target(
+  #   name = separate_layer_rankmap,
+  #   command = run_zonation(feature_list = c(mall_reprojected,
+  #                                           gadw_reprojected,
+  #                                           nopi_reprojected,
+  #                                           bwte_reprojected,
+  #                                           nsho_reprojected,
+  #                                           canv_reprojected,
+  #                                           redh_reprojected),
+  #                          scenario_name = "separate_layer_all",
+  #                          zonation_mode = "CAZMAX"),
+  #   format = "file"
+  # ),
+  # 
+  # tar_target(
+  #   name = separate_layer_tl,
+  #   command = generate_target_landscape(rankmap_path = separate_layer_rankmap,
+  #                                       scenario_name = "separate_layer_all",
+  #                                       threshold = 0.74,
+  #                                       min_poly = 156000000,
+  #                                       max_hole = 70000000,
+  #                                       smooth = 8),
+  #   format = "file"
+  # ),
+  # 
+  # # tar_target(
+  # #   name = separate_layer_tl_plot,
+  # #   command = plot_target_landscape(tl_path = separate_layer_tl,
+  # #                                   old_tl_path = tl_old,
+  # #                                   width = 6,
+  # #                                   height = 6,
+  # #                                   res = 300,
+  # #                                   units = "in",
+  # #                                   scenario_name = "separate_layer_all")
+  # # ),
+  # 
+  # ###### Guild Level Scenario #####
+  # # i.e., a layer for dabblers, a layer for divers, and a layer for NOPI
+  # tar_target(
+  #   name = guild_level_rankmap,
+  #   command = run_zonation(feature_list = c(dabblers_stacked,
+  #                                           divers_stacked,
+  #                                           nopi_reprojected),
+  #                          scenario_name = "guild_level",
+  #                          zonation_mode = "CAZMAX"),
+  #   format = "file"
+  # ),
+  # 
+  # tar_target(
+  #   name = guild_level_tl,
+  #   command = generate_target_landscape(rankmap_path = guild_level_rankmap,
+  #                                       scenario_name = "guild_level",
+  #                                       threshold = 0.74,
+  #                                       min_poly = 156000000,
+  #                                       max_hole = 70000000,
+  #                                       smooth = 8),
+  #   format = "file"
   # )
+  # 
+  # # tar_target(
+  # #   name = guild_level_tl_plot,
+  # #   command = plot_target_landscape(tl_path = guild_level_tl,
+  # #                                   old_tl_path = tl_old,
+  # #                                   width = 6,
+  # #                                   height = 6,
+  # #                                   res = 300,
+  # #                                   units = "in",
+  # #                                   scenario_name = "guild_level")
+  # # )
   
 )
