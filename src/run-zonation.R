6d06e77abfa41f640c098d732a145f6462cc9c63
Brandon Edwards <b_edwards@ducks.ca>
2025-05-20 08:52:33 -0500

    forgot to actually update the hard-coded zonation run here

--- src/run-zonation.R
+++ src/run-zonation.R
@@ -56,8 +56,8 @@ run_zonation <- function(feature_list = NULL,
   cmd_string <- paste0(cmd_string, "@PATH=C:\\Program Files (x86)\\Zonation5;%PATH%\n")
   cmd_string <- paste0(cmd_string, "z5_16bit ")
   cmd_string <- ifelse(!is.null(feature_weights),
-                       paste0(cmd_string, "-w --mode=CAZMAX "),
-                       paste0(cmd_string, "--mode=CAZMAX "))
+                       paste0(cmd_string, "-w --mode=", zonation_mode, " "),
+                       paste0(cmd_string, "--mode=", zonation_mode, " "))
   cmd_string <- paste0(cmd_string, "data/generated/zonation/", scenario_name, "_", zonation_mode, ".z5 ")
   cmd_string <- paste0(cmd_string, "output/zonation/", scenario_name, "_", zonation_mode, "\n@pause")
   writeLines(cmd_string, cmd_file)
